8e3654fcbed8f3374670569eb7f6ca629ec49c42
spencerho777 <42759103+spencerho777@users.noreply.github.com>
2023-03-02 13:20:30 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -1,56 +1,31 @@
-CPATH=".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar"
+CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 
-rm ListExamples*
-rm *.class
 rm -rf student-submission
-rm errorFile.txt
 git clone $1 student-submission
-echo 'Finished cloning'
-
 cd student-submission
-echo 'In submission directory'
-
-cp ListExamples.java ..
-cd ..
-GRADE=100
-
 if [[ -f ListExamples.java ]]
-then
-    echo 'ListExamples found'
-else
-    echo 'Missing File ListExamples.java'
-    GRADE-=100
+then 
+    echo "ListExamples found"
+else 
+    echo "ListExamples.java missing"
+    exit 1
 fi
 
-FILTER=`grep -c -i "static List<String> filter(List<String> list, StringChecker sc)" ListExamples.java`
-MERGE=`grep -c -i "static List<String> merge(List<String> list1, List<String> list2)" ListExamples.java`
 
-if [[ $FILTER -eq 1 ]]
-then
-    echo "Filter method found."
+cp ../TestListExamples.java ./
+javac -cp .:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar *.java > javac-errors.txt
+if [[ $? -eq 0 ]]
+then 
+    echo "Success Compile"
 else
-    echo "Filter method not found."
-    GRADE-=50
+    echo "Failed Compile"
 fi
 
-if [[ $MERGE -eq 1 ]]
-then
-    echo "Merge method found."
-else
-    echo "Merge method not found."
-    GRADE-=50
-fi
+java -cp .:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples > test_results.log
 
+cat test_results.log
+echo 'Done cloning'
+
+cp student-submission/ListExamples.java ./
 javac -cp $CPATH *.java
-java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > errorFile.txt 2>&1
-
-if grep -q "OK" errorFile.txt
-then
-    echo "Great job!"
-    echo "Grade: 100/100"
-else
-    FAILURES=`grep -c "Failure" errorFile.txt`
-    FAILURES*=25
-    GRADE-=FAILURES
-    echo "Grade: "$GRADE"/100"
-fi
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples
